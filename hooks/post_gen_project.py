0cb96daee253cd9e55505fe888cb464a7cf0dd18
Derek Sudduth <derek_sudduth@symantec.com>
2019-06-02 13:40:05 -0700

    initial check-in

---
+++ hooks/post_gen_project.py
@@ -0,0 +1,14 @@
+"""Post-creation hook"""
+
+import os
+
+PROJECT_DIRECTORY = os.path.realpath(os.path.curdir)
+
+
+def remove_file(filepath):
+    os.remove(os.path.join(PROJECT_DIRECTORY, filepath))
+
+
+if __name__ == '__main__':
+    if '{{ cookiecutter.use_ci}}'.lower() != 'y':
+        remove_file('Jenkinsfile')
